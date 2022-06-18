b4728a1c867fbd0e60dcb48b5ce54c1ffa9af3ea
DELL-02\ThinThinMoe <scm.thinthinmoe@gmail.com>
2022-06-18 19:13:50 +0630

    register, login, logout

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 
 // Route::get('/posts', [PostController::class, 'index']);
 // Route::get('/posts/create', [PostController::class, 'create']);
@@ -12,5 +14,13 @@ use App\Http\Controllers\PostController;
 // Route::get('/posts/show/{id}', [PostController::class, 'show']);
 // Route::get('/posts/delete/{id}', [PostController::class, 'destroy']);
 
+Route::get('/', [PostController::class, 'index']);
 Route::resource('posts', PostController::class);
 Route::resource('categories', CategoryController::class);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
+
+Route::get('login', [LoginController::class, 'create']);
+Route::post('login', [LoginController::class, 'store']);
+Route::get('logout', [LoginController::class, 'destroy']);
